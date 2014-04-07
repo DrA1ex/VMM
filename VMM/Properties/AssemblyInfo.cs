51e05ee774fb2220715aa375cda9eeefaa9bad3a
Alexander <a1ex.chaos.exe@gmail.com>
2014-04-07 20:33:40 +0600

    Version 0.9.3.1

--- VMM/Properties/AssemblyInfo.cs
+++ VMM/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Vk Music Manager")]
+[assembly: AssemblyTitle("Менеджер музыки Вконтакте")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.3.0")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: AssemblyVersion("0.9.3.1")]
+[assembly: AssemblyFileVersion("0.9.3.1")]
