a1a509c02771a556daf4f36e9186282597e8226b
Alexander <a1ex.chaos.exe@gmail.com>
2014-04-07 13:54:34 +0600

    Added tooltips for sorting method button

---
+++ VMM/Converters/SortingMethodDescriptionConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VMM.Converters
+{
+    internal class SortingMethodDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool descending = value is bool && (bool)value;
+            return descending ? "По убыванию" : "По возрастанию";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
